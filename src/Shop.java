6741a0e10494c5ea7a2b9c02155322d70d0b8486
Alexandernevsk <Alexandertorreman@gmail.com>
2023-08-21 14:06:57 +0200

    New test-branch

---
+++ src/Shop.java
@@ -0,0 +1,11 @@
+public class Shop {
+    private double price;
+    private String name;
+    private String description;
+
+    public Shop(double price, String name, String description) {
+        this.price = price;
+        this.name = name;
+        this.description = description;
+    }
+}
--- src/TestClass.java
+++ src/TestClass.java
@@ -5,13 +5,5 @@ public class TestClass {
     }
 
     enum Food {Frikandel, Kroket, Bitterbal, Mexicano}
-    public static void main(String[] args) {
-        ArrayList<Food> foods = new ArrayList<>();
-        foods.add(Food.Frikandel);
-        foods.add(Food.Bitterbal);
-        foods.add(Food.Mexicano);
-        for(Food food : foods){
-            System.out.println(food);
-        }
-    }
+
 }
