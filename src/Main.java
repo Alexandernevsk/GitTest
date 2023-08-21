4b7a4958d0ad56c9929b36d4f5ce781c018036eb
Alexandernevsk <Alexandertorreman@gmail.com>
2023-08-21 14:15:23 +0200

    master-branch added new password class

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test branch v2.0");
-
-        ArrayList<TestClass.Food> foods = new ArrayList<>();
-        ArrayList<TestClass.Gem> gems = new ArrayList<>();
-
-        gems.add(TestClass.Gem.Sapphire);
-        gems.add(TestClass.Gem.Emerald);
-        gems.add(TestClass.Gem.Diamond);
-
-        gems.forEach(c -> System.out.println(c));
-
-        foods.add(TestClass.Food.Frikandel);
-        foods.add(TestClass.Food.Bitterbal);
-        foods.add(TestClass.Food.Mexicano);
-
-        for(TestClass.Food food : foods){
-            System.out.println(food);
-        }
-
-        Shop item = new Shop(2.0, "Banaan", "Lekker rijk aan magnesium");
-        System.out.println(item.getName());
+        System.out.println("test branch for test branch v1.0");
     }
 }
